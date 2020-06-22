65e2be5d14c58b982664bff668c8b7765766a5ae
Dominik Wątor <domiwator16@gmail.com>
2020-06-22 12:19:49 +0200

    Added planed functions to do

---
+++ Battleship.py
@@ -0,0 +1,50 @@
+def game():
+
+    pass
+
+
+def place_your_ship(ship):
+    # zaznacza ustawienie statków z wykorzystaniem is_possible_to_mark()
+    # musi zapisać współrzędne położenia statku w liście
+    # parametr ship będzie przyjmował długość statku
+
+    pass
+
+
+def is_possible_to_mark(board):
+    # na etapie ustawiania statków sprawdza czy statki się ze sobą nie stykają
+
+    pass
+
+
+def display_board(board):
+    # pokazuje zmiany na planszy
+
+    pass
+
+
+def init_board():
+    # inicjuje pustą planszę
+
+    pass
+
+
+def shoot():
+
+    pass
+
+
+def mark_shoot_place():
+    # 0 indicates an undiscovered tile
+    # M indicates a missed shot
+    # H indicates a hit ship part
+    # S indicates a sunk ship part
+
+    pass
+
+
+def end_game():
+    # wypisuje zwycięzcę i kończy grę
+    # wychodzi z pętli
+
+    pass
